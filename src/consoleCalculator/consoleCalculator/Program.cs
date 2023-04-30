30893dfff423e7586734cfb9a86d69757f0179d1
Shamir Pontes <shamirpontes77@gmail.com>
2023-04-30 15:03:22 -0300

    Alteração das variáveis que armazenar o valor inserido

--- src/consoleCalculator/consoleCalculator/Program.cs
+++ src/consoleCalculator/consoleCalculator/Program.cs
@@ -38,14 +38,14 @@ namespace consoleCalculator
             Console.Clear();
 
             Console.WriteLine("Primeiro Valor: ");
-            float v1 = float.Parse(Console.ReadLine());
+            float valor1 = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Segundo Valor; ");
-            float v2 = float.Parse(Console.ReadLine());
+            float valor2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine(" ");
 
-            float resultado = v1 + v2;
+            float resultado = valor1 + valor2;
             Console.WriteLine($"O resultado da soma é: {resultado} ");
             Console.ReadKey();
             Menu();
@@ -56,12 +56,12 @@ namespace consoleCalculator
             Console.Clear();
 
             Console.WriteLine("Primeiro Valor: ");
-            float v1 = float.Parse (Console.ReadLine());
+            float valor1 = float.Parse (Console.ReadLine());
 
             Console.WriteLine("Segundo Valor: ");
-            float v2 = float.Parse(Console.ReadLine());
+            float valor2 = float.Parse(Console.ReadLine());
 
-            float resultado = v1 - v2;
+            float resultado = valor1 - valor2;
             Console.WriteLine($" O resultado da subtração é: {resultado}");
             Console.ReadKey();
             Menu();
@@ -73,12 +73,12 @@ namespace consoleCalculator
             Console.Clear();
 
             Console.WriteLine("Primeiro Valor: ");
-            float v1 = float.Parse(Console.ReadLine());
+            float valor1 = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Segundo Valor: ");
-            float v2 = float.Parse(Console.ReadLine());
+            float valor2 = float.Parse(Console.ReadLine());
 
-            float resultado = v1/v2;
+            float resultado = valor1/valor2;
             Console.WriteLine($" O resultado da divisão é: {resultado}");
             Console.ReadKey();
             Menu();
@@ -89,12 +89,12 @@ namespace consoleCalculator
             Console.Clear();
 
             Console.WriteLine("Primeiro Valor: ");
-            float v1 = float.Parse(Console.ReadLine());
+            float valor1 = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Segundo Valor: ");
-            float v2 = float.Parse(Console.ReadLine());
+            float valor2 = float.Parse(Console.ReadLine());
 
-            float resultado = v1 * v2;
+            float resultado = valor1 * valor2;
             Console.WriteLine($" O resultado da multiplicação é: {resultado}");
             Console.ReadKey();
             Menu();
